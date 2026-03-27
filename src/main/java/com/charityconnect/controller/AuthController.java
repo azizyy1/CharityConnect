ad5055357d72165197592e53888f69f53af22cfe
Azizi Hajar <hajaarazizi73@gmail.com>
2026-03-27 19:16:35 +0100

    update

--- src/main/java/com/charityconnect/controller/AuthController.java
+++ src/main/java/com/charityconnect/controller/AuthController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,7 +43,6 @@ public class AuthController {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.ROLE_USER);
         user.setEnabled(true);
-
         userRepository.save(user);
         return "redirect:/login?registered";
     }
