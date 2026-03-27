989edd742a6064609e850c295e1a8b921b50ead5
Azizi Hajar <hajaarazizi73@gmail.com>
2026-03-27 01:15:07 +0100

    Replace project with new version

---
+++ src/main/java/com/charityconnect/charityconnect/CharityconnectApplication.java
@@ -0,0 +1,13 @@
+package com.charityconnect.charityconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charityconnect/charityconnect/CharityconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charityconnect.charityconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
